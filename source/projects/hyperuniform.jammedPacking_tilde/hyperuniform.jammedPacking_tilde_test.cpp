717e1ec10aa709b0b1a50cfc11b3fa36f4a5d09b
danBennettDev <skjolbrot@gmail.com>
2018-06-20 15:11:05 +0100

    fixed cmake issue

--- source/projects/hyperuniform.jammedPacking_tilde/hyperuniform.jammedPacking_tilde_test.cpp
+++ source/projects/hyperuniform.jammedPacking_tilde/hyperuniform.jammedPacking_tilde_test.cpp
@@ -3,7 +3,7 @@
 // Usage of this file and its contents is governed by the MIT License
 
 #include "c74_min_unittest.h"		// required unit test header
-#include "hyperuniform.jammedPacking~.cpp"	// need the source of our object so that we can access it
+#include "hyperuniform.jammedPacking_tilde.cpp"	// need the source of our object so that we can access it
 
 // Unit tests are written using the Catch framework as described at
 // https://github.com/philsquared/Catch/blob/master/docs/tutorial.md
